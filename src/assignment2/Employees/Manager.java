d7de508f4f06df62f890f6791b3c1c030a596ec1
s19181 <s19181@pjwstk.edu.pl>
2020-11-01 01:53:57 +0100

    made working (probably) getAllSubordinates() method

--- src/assignment2/Employees/Manager.java
+++ src/assignment2/Employees/Manager.java
@@ -3,13 +3,17 @@ package assignment2.Employees;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public final class Manager extends Worker {
 
 	// attributes
 	// * subordinates (a list of immediate subordinates)
 	// * all subordinates (derived --- i.e. calculated on the fly --- a list of subordinates in all hierarchy)
-	private LinkedList<Employee> _subordinates;
+	private List<Employee> _subordinates;
+	private List<Employee> _allSubordinates;
+
 	public Manager(String firstName, String lastName, LocalDate birthDate, BigDecimal salary, Manager manager,
 				   LocalDate employmentDate, BigDecimal bonus, LinkedList<Employee> subordinates) {
 
@@ -17,17 +21,18 @@ public final class Manager extends Worker {
 		_subordinates = subordinates;
 	}
 
-	public void setSubordinates(LinkedList<Employee> _subordinates) {
+	public void setSubordinates(List<Employee> _subordinates) {
 		this._subordinates = _subordinates;
 	}
 
-	public LinkedList<Employee> getAllSubordinates() {
-		LinkedList<Employee> list = new LinkedList<>();
-		for (Employee e : _subordinates) {
-			if (e.getClass() == Manager.class)
-				((Manager) e).getAllSubordinates();
-			list.add(e);
-		}
-		return list;
+	public List<Employee> getAllSubordinates() {
+//		LinkedList<Employee> list = new LinkedList<>();
+//		for (Employee e : _subordinates) {
+//			if (e.getClass() == Manager.class)
+//				((Manager) e).getAllSubordinates();
+//			list.add(e);
+//		}
+//		return list;
+		return _allSubordinates.stream().filter(e -> e.getManager() == this).collect(Collectors.toList());
 	}
 }
